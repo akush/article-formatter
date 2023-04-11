b5956df085935aff7c8dacb034170e4ff7bc9f9e
Abhinav Kushwaha <abhinav@ipsator.com>
2023-04-11 23:46:24 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -26,62 +26,38 @@ function App() {
   // const [contentType, setContentType] = useState('');
   const [content, setContent] = useState('');
   const [faqItems, setFaqItems] = useState([]);
-
-  const [metaTags, setMetaTags] = useState({
-    title: '',
-    description: '',
-    keywords: '',
-    image: ''
-  });
-
+  const [metaTags, setMetaTags] = useState({});
   const [article, setArticle] = useState("");
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
-    setArticle(`<div class="content>\n${content}</div>
+    setArticle(`<div class="content">\n${content}</div>
 <div class="metatags" style="visibility: hidden; height: 0 !important; width: 0 !important;">\n<pre>\n${JSON.stringify(metaTags, null, 2)}\n</pre>\n</div>
 <div class="faqItems" style="visibility: hidden; height: 0 !important; width: 0 !important;">\n<pre>\n${JSON.stringify(faqItems, null, 2)}\n</pre>\n</div>`)
   }, [content, faqItems, metaTags]);
 
-  const handleMetaTagsChange = (newMetaTags) => {
-    setMetaTags(newMetaTags);
+  const copyHandler = () => {
+    console.log("Copied to clipboard");
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
   };
-
-  // const handleChange = (e) => {
-  //   const selectedType = e.target.value;
-  //   setContentType(selectedType);
-  // };
 
   return (
     <div className='w-full p-8 mx-auto'>
       <h1 style={{ textAlign: "center" }}>Create blog/page content</h1>
-
-      <MetaTags onMetaTagsChange={handleMetaTagsChange} />
-
+      <MetaTags onMetaTagsChange={setMetaTags} />
       <div className='flex flex-col md:flex-row gap-8 content-start'>
         <div className='w-full md:w-1/2'>
-          {/* <div className="flex items-center my-4">
-            <label htmlFor="content-type" className="w-1/2">
-              Content Type:
-            </label>
-            <select
-              id="content-type"
-              className="border rounded w-full h-12 p-2"
-              value={contentType}
-              onChange={handleChange}
-            >
-              <option value="blog">Blog</option>
-              <option value="page">Page</option>
-            </select>
-          </div> */}
-          <Editor onContentChange={content => setContent(content)} />
-          <FAQ onFaqChange={faqItems => setFaqItems(faqItems)} />
+          <Editor onContentChange={setContent} />
+          <FAQ onFaqChange={setFaqItems} />
         </div>
         <div className='w-full md:w-1/2'>
           <div className='flex justify-between items-center'>
             <h2>Output</h2>
-
-            <CopyToClipboard text={article}>
-              <button className='bg-green-500 text-white rounded p-1 h-8'>Copy HTML</button>
+            <CopyToClipboard text={article} onCopy={copyHandler}>
+              <button className='bg-green-500 text-white rounded py-1 px-4 h-8'>{copied ? "Copied" : "Copy HTML"}</button>
             </CopyToClipboard>
           </div>
           <pre className='bg-gray-100 rounded p-4'>{article}</pre>
